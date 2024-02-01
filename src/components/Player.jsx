53c64b100e9daa5a7897459d8321b7be7af20eda
Yaron Peres <yaronp16@gmail.com>
2024-02-01 18:42:35 +0100

    change

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,16 +1,29 @@
 import { useState } from "react";
 
-function player({ name, symbol }) {
+function player({ initialName, symbol }) {
   const [editing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   function handleClick() {
     setIsEditing((check) => !check);
   }
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+    //
+  }
   return (
     <li>
       <span className="player">
-        {!editing && <span className="player-name">{name}</span>}
-        {editing && <input type="text" required />}
+        {!editing && <span className="player-name">{playerName}</span>}
+        {editing && (
+          <input
+            type="text"
+            required
+            onChange={handleChange}
+            value={playerName}
+          />
+        )}
         <span className="playe-symbol">{symbol}</span>
       </span>
       <button onClick={handleClick}>{!editing ? "Edit" : "Save"}</button>
